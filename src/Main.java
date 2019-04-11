6058d40f4760c8ab9bb892b62bc2956d2db3b952
Sabrina <slanza.ieu2015@student.ie.edu>
2019-04-11 03:35:58 +0200

    second commit

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,20 @@
+import java.text.DecimalFormat;
+import java.text.NumberFormat;
+
 public class Main {
 
-    public static void main(String[] args) {
-        System.out.println("Hello World!");
+    public static void main(String [] args) {
+        double d = 0.0;
+
+        NumberFormat format = new DecimalFormat();
+        format.setMaximumFractionDigits(1);
+
+        while (d < 1.0) {
+            d += 0.1;
+            d = Double.parseDouble(format.format(d));
+            System.out.println(d);
+        }
+
+        System.out.println("d is 1");
     }
 }
